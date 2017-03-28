9f0cb72a7694b32722d63ef58d493b60a3a633b9
Rosalind Ellis <rnellis@brandeis.edu>
2017-03-28 15:02:13 -0400

    New 3x3 neural network
    3x3 convolutions, 32 features then 64 features.
    Worse accuracy

--- Predicts.py
+++ Predicts.py
@@ -31,11 +31,13 @@ def avg_pool_2x2(x):
 x = tf.placeholder(tf.float32, [None, 784]) #input
 y_ = tf.placeholder(tf.float32, [None, 10])
 W_conv1 = weight_variable([5, 5, 1, 32])
+# W_conv1 = weight_variable([3, 3, 1, 32])
 b_conv1 = bias_variable([32])
 x_image = tf.reshape(x, [-1,28,28,1])
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = avg_pool_2x2(h_conv1)
 W_conv2 = weight_variable([5, 5, 32, 64])
+# W_conv2 = weight_variable([3, 3, 32, 64])
 b_conv2 = bias_variable([64])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = avg_pool_2x2(h_conv2)
